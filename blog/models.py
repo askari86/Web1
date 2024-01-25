635df044c67c5bee9d40b6c1d740bd2633c7df14
askari86 <154256345+askari86@users.noreply.github.com>
2024-01-25 13:40:55 +0330

    detabase
    create & work with deta base

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+#makemigrations و migrate
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    #image
+    #author
+    test=models.CharField(max_length=255)
+    content=models.TextField()
+    #tag
+    #category
+    counted_views=models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    publish_date=models.DateTimeField(null=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return '{} {}'.format(self.test,self.id)
